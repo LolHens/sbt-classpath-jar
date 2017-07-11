a323f77eeac34724ae7fd3e1bbcfcfad8c60f8de
LolHens <pierrekisters@gmail.com>
2017-07-11 14:57:26 +0200

    resolve name conflict

--- src/main/scala/org/lolhens/sbt/PackagedClasspathJarPlugin.scala
+++ src/main/scala/org/lolhens/sbt/PackagedClasspathJarPlugin.scala
@@ -1,5 +1,7 @@
 package org.lolhens.sbt
 
+import java.util.jar.Attributes
+
 import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
 import com.typesafe.sbt.packager.universal.UniversalPlugin
 import sbt.Keys.{mappings, target}
@@ -8,9 +10,11 @@ import sbt.{AutoPlugin, IO, _}
 /**
   * Created by u016595 on 11.07.2017.
   */
-class ClasspathJarPlugin extends AutoPlugin {
+class PackagedClasspathJarPlugin extends AutoPlugin {
 
   object autoImport {
+    val classpathJarName: SettingKey[String] = SettingKey[String]("classpathJarName")
+
     val additionalClasspathEntries: SettingKey[Seq[String]] = SettingKey[Seq[String]]("additionalClasspathEntries")
   }
 
@@ -21,14 +25,26 @@ class ClasspathJarPlugin extends AutoPlugin {
   import autoImport._
 
   override lazy val projectSettings = Seq(
+    classpathJarName := "classpath.jar",
+
     scriptClasspath := {
       val manifest = new java.util.jar.Manifest()
-      manifest.getMainAttributes.putValue("Class-Path", (scriptClasspath.value ++ additionalClasspathEntries.value).mkString(" "))
-      val classpathJar = (target in Universal).value / "lib" / "classpath.jar"
+
+      manifest.getMainAttributes.putValue(
+        Attributes.Name.CLASS_PATH.toString,
+        (scriptClasspath.value ++ additionalClasspathEntries.value).mkString(" ")
+      )
+
+      val classpathJar = (target in Universal).value / "lib" / classpathJarName.value
+
       IO.jar(Seq.empty, classpathJar, manifest)
-      Seq("classpath.jar")
+
+      Seq(classpathJarName.value)
     },
 
-    mappings in Universal += ((target in Universal).value / "lib" / "classpath.jar", "lib/classpath.jar")
+    mappings in Universal += (
+      (target in Universal).value / "lib" / classpathJarName.value,
+      s"lib/${classpathJarName.value}"
+    )
   )
 }
